4e1af72eacc0b087b53a189a851b2aa92ce8dc72
Matthew-JDN <leiprtla@gmail.com>
2025-11-09 02:30:43 +0800

    final

--- src/App.jsx
+++ src/App.jsx
@@ -11,40 +11,6 @@ import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
-    /*
-    <>
-      <div>
-        <div className="flex h-64 p-4 m-4 border-2 border-blue-400 justify-center">
-          <Cat />
-          <Cat />
-          <Cat />
-        </div>
-        <div className="flex h-64 p-4 m-4 border-2 border-red-400 justify-between">
-          <Cat />
-          <Cat />
-          <Cat />
-        </div>
-        <div className="flex h-64 p-4 m-4 border-2 border-green-400 justify-center items-center">
-          <Cat />
-          <Cat />
-          <Cat />
-        </div>
-        <div className="flex h-64 p-4 m-4 border-2 border-purple-400 justify-around items-end">
-          <Cat />
-          <Cat />
-          <Cat />
-        </div>
-        <div className="flex h-64 p-4 m-4 border-2 border-orange-400 justify-between items-center">
-          <Cat />
-          <Cat />
-          <Cat />
-        </div>
-      </div>
-    </>
-    */
-    //<Container />
-
-    //<Layout />
     <div className="h-screen flex flex-col">
       <Header />
       <SearchSection />
